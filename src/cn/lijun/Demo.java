0a115c064a54bf02392ab57292431d84e1c2f7a7
jundou6 <15138956487@163.com>
2019-09-04 11:51:33 +0800

    李俊第一个版本1

--- src/cn/lijun/Demo.java
+++ src/cn/lijun/Demo.java
@@ -6,6 +6,6 @@ package cn.lijun;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("sadjfjkl");
+        System.out.println("sadjfjklggg");
     }
 }

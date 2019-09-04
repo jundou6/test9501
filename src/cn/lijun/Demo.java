c3aa62a3e38efead2e60cb7a54b7444a3ebb6650
jundou6 <15138956487@163.com>
2019-09-04 11:41:04 +0800

    李俊第一个版本

---
+++ src/cn/lijun/Demo.java
@@ -0,0 +1,11 @@
+package cn.lijun;
+
+/**
+ * @author lijun
+ * @date 2019/9/4 11:36
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("sadjfjkl");
+    }
+}
